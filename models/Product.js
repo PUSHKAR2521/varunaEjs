eabf2b9d9d994308ada20305b29d4d0dfe07daa6
B A C <117440896+PUSHKAR2521@users.noreply.github.com>
2025-02-21 10:38:25 +0000

    Admin Page Updates Major

--- models/Product.js
+++ models/Product.js
@@ -1,17 +1,32 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    name: String,
-    description: String,
-    input: String,
-    salient_features: [String],
-    applications: [String],
-    material: String,
     model: String,
     motor_rating: String,
     stages: Number,
     head_meters: [Number],
     discharge_lpm: [Number],
+    description: String,
+    input: String,
+    salient_features: [String],
+    applications: [String],
+    material: String,
+    images: [String], 
+    motor_details: {
+        type: Map,
+        of: String
+    },
+    pump_details: {
+        type: Map,
+        of: String
+    },
+    group: String,  // New Fields
+    hertz: String,
+    suction_size: String,
+    delivery_size: String,
+    voltage: String,
+    power_supply: String,
+    type: String
 });
 
 module.exports = mongoose.model('Product', productSchema);
